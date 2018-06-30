8faafdcae97c7904e5a07a7285b3a169a168e61d
Michael R Fleet <f1337@f1337.us>
2018-06-30 13:27:59 -0400

    atreus62:f1337 swap slash and backslash

--- keyboards/atreus62/keymaps/f1337/README.md
+++ keyboards/atreus62/keymaps/f1337/README.md
@@ -1,2 +1,6 @@
-docker run -e keymap=f1337 -e keyboard=atreus62 --rm -v ('pwd'):/qmk:rw edasque/qmk_firmware
-/usr/local/CrossPack-AVR/bin/avrdude -p atmega32u4 -P /dev/tty.usbmodem1411 -c avr109  -U flash:w:atreus62_f1337.hex
+1. Install [CrossPack AVR](https://obdev.at/products/crosspack/download.html)
+1. `docker run -e keymap=f1337 -e keyboard=atreus62 --rm -v ('pwd'):/qmk:rw edasque/qmk_firmware`
+1. Queue up the following in your terminal, but do not hit Enter yet: `/usr/local/CrossPack-AVR-20131216/bin/avrdude -p atmega32u4 -P /dev/tty.usbmodem1411 -c avr109  -U flash:w:atreus62_f1337.hex`
+1. Tap the reset button on the back of the Atreus62.
+1. Quickly hit "Enter" to submit the command you queued up.
+1. Profit.
--- keyboards/atreus62/keymaps/f1337/keymap.c
+++ keyboards/atreus62/keymaps/f1337/keymap.c
@@ -16,10 +16,10 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_DVORAK] = { /* dvorak */
-        { KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   _______, KC_6,   KC_7,    KC_8,    KC_9,  KC_0,    KC_SLSH     },
+        { KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   _______, KC_6,   KC_7,    KC_8,    KC_9,  KC_0,    KC_BSLS     },
         { KC_TAB,   KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,   _______, KC_F,   KC_G,    KC_C,    KC_R,  KC_L,    KC_EQL      },
         { KC_LCTL,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,   _______, KC_D,   KC_H,    KC_T,    KC_N,  KC_S,    KC_MINS     },
-        { KC_LSFT,  KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,   KC_ESC,  KC_B,   KC_M,    KC_W,    KC_V,  KC_Z,    KC_BSLS     },
+        { KC_LSFT,  KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,   KC_ESC,  KC_B,   KC_M,    KC_W,    KC_V,  KC_Z,    KC_SLSH     },
         { MO(_FN),  XXXXXXX, KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_BSPC, KC_ENT, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, TG(_GAMING) }
     },
 
