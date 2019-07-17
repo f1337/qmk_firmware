e660e6d0afff5afde5ed0765f09f76b26edb1af2
Michael R Fleet <f1337@f1337.us>
2019-07-17 00:18:11 -0400

    multi layer toggle with tap dance

---
+++ keyboards/atreus62/keymaps/f1337/config.h
@@ -0,0 +1,6 @@
+#include "../../config.h"
+
+#define MOUSEKEY_MAX_SPEED 6
+#define MOUSEKEY_TIME_TO_MAX 40
+
+#define TAPPING_TERM 200
--- keyboards/atreus62/keymaps/f1337/keymap.c
+++ keyboards/atreus62/keymaps/f1337/keymap.c
@@ -12,9 +12,83 @@
 #define _MK 2
 #define _FN 3
 
-// Fillers to make layering more clear
-#define _______ KC_TRNS
-#define XXXXXXX KC_NO
+// tap-dance
+// see https://github.com/qmk/qmk_firmware/blob/master/docs/feature_tap_dance.md#example-5-using-tap-dance-for-advanced-mod-tap-and-layer-tap-keys
+// and https://www.reddit.com/r/olkb/comments/a69k5q/qmk_multilayer_taphold/ebtebry/
+#define TD_LAYER TD(TD_LAYER_SWAP)
+
+typedef struct {
+  bool is_press_action;
+  int state;
+} tap;
+
+static tap tap_state = {
+  .is_press_action = true,
+  .state = 0
+};
+
+enum tapdance_id {
+  TD_LAYER_SWAP = 0,
+};
+
+enum {
+  SINGLE_TAP = 1,
+  SINGLE_HOLD = 2,
+  DOUBLE_TAP = 3
+};
+
+int cur_dance (qk_tap_dance_state_t *state) {
+  if (state->count == 1) {
+    if (state->interrupted || !state->pressed)  return SINGLE_TAP;
+    else return SINGLE_HOLD;
+  }
+  else if (state->count == 2) {
+    if (state->interrupted || !state->pressed) return DOUBLE_TAP;
+    else return 4;
+  }
+  else return 4;
+}
+
+void tap_finished (qk_tap_dance_state_t *state, void *user_data) {
+  tap_state.state = cur_dance(state);
+  uint8_t layer = biton32(layer_state);
+
+  switch (tap_state.state) {
+    case SINGLE_HOLD:
+      layer_on(_FN);
+      layer_off(_MK);
+      break;
+    case SINGLE_TAP:
+      if (layer == _MK) {
+        layer_off(_MK);
+      } else {
+        layer_on(_MK);
+      }
+      break;
+    case DOUBLE_TAP:
+      if (layer == _GAMING) {
+        layer_off(_GAMING);
+      } else {
+        layer_on(_GAMING);
+      }
+      break;
+  }
+}
+
+void tap_reset (qk_tap_dance_state_t *state, void *user_data) {
+  switch (tap_state.state) {
+    case SINGLE_HOLD:
+      layer_off(_FN);
+      break;
+  }
+  tap_state.state = 0;
+}
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [TD_LAYER_SWAP] = ACTION_TAP_DANCE_FN_ADVANCED (NULL, tap_finished, tap_reset)
+};
+
+// end tap-dance
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_DVORAK] = LAYOUT( /* dvorak */
@@ -22,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,                        KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_EQL,
         KC_LCTL,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                        KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS,
         KC_LSFT,  KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,                        KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_SLSH,
-        MO(_FN),  TG(_MK), KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  KC_ESC,   KC_BSPC,  KC_ENT,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, TG(_GAMING)
+        TD_LAYER, _______, KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  KC_ESC,   KC_BSPC,  KC_ENT,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______
     ),
 
     [_GAMING] = LAYOUT( /* gaming overrides, macros */
@@ -34,11 +108,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_MK] = LAYOUT( /* mouse keys */
-        _______,  KC_ACL0, KC_ACL1, KC_ACL2, _______, _______,                     _______, _______, _______, _______, _______, _______,
-        _______,  _______, _______, KC_WH_U, _______, _______,                     _______, _______, KC_MS_U, _______, _______, _______,
-        _______,  _______, KC_WH_L, KC_WH_D, KC_WH_R, _______,                     _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______,
-        _______,  _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
-        _______,  _______, _______, _______, _______, _______, _______,  _______,  KC_BTN1, KC_BTN1, _______, _______, _______, _______
+        _______,  KC_ACL0, KC_ACL1, KC_ACL2, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        _______,  XXXXXXX, XXXXXXX, KC_WH_U, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, KC_MS_U, XXXXXXX, XXXXXXX, XXXXXXX,
+        _______,  XXXXXXX, KC_WH_L, KC_WH_D, KC_WH_R, XXXXXXX,                     XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_R, XXXXXXX, XXXXXXX,
+        _______,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        _______,  _______, _______, _______, _______, XXXXXXX, _______,  KC_BTN1,  KC_BTN2, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______
     ),
 
     [_FN] = LAYOUT( /* FN keys, symbols */
@@ -57,7 +131,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // ),
 
 };
-
-// const uint16_t PROGMEM fn_actions[] = {
-
-// };
